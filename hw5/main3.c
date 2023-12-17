08f2bf8cdca2eafe886753468be04cfdbb6adc9c
PEI1111111 <151009469+PEI1111111@users.noreply.github.com>
2023-12-17 20:04:21 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -6,7 +6,8 @@ int main()
  for(c = 1; c <= 72; ++c){
      int i = (c-1)/9+2;
      int j =(c-1)%9+1;
-     printf("%d x %d\n", i, j, i*j);
+     printf("%d*%d=%d\t", i, j, i*j);
  }
+ printf("\n");
     return 0;
 }

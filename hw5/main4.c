7b0fba030a8f93f9df424c091512f7eaaeb6424d
PEI1111111 <151009469+PEI1111111@users.noreply.github.com>
2023-12-20 10:44:43 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -9,7 +9,7 @@ int main()
     int thousand = i/1000%10;
     int upper = i/10000;
 
-    printf("switch result = %d",upper*10000+unit*1000+hundred*100+ten*10+thousand);
+    printf("%d",upper*10000+unit*1000+hundred*100+ten*10+thousand);
 
     return 0;
 
